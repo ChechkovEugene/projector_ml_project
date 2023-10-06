f757ef184b21d71bcc200e75e075ab0ad4a1f9fd
Eugene Chechkov <Chechkov.Eugene@gmail.com>
2023-10-06 14:57:44 +0300

    Fix predictor

--- src/predictor/wine_price_predictor.py
+++ src/predictor/wine_price_predictor.py
@@ -24,14 +24,23 @@ class WinePricePredictor:
         feature_names = self._model["reg"].get_booster().feature_names
 
         numerical = samples_df.select_dtypes(exclude=['object']).columns
+
+        samples_df['Grape'] = samples_df['Grape'].str.split(',')
+
+        def split_list(row):
+            return pd.Series(row['Grape'])
+
+        new_df = samples_df.apply(split_list, axis=1).rename(columns=lambda x: f"Grape_{x+1}")
+        samples_df = pd.concat([samples_df, new_df], axis=1)
+        samples_df.drop('Grape', axis=1, inplace=True)
+
         category = samples_df.select_dtypes(include=['object']).columns
-
         samples_df[category] = samples_df[category].astype('category')
-
         samples_df = pd.get_dummies(samples_df, columns=category)
         samples_df = samples_df.reindex(columns = feature_names, fill_value=False)
 
         samples_df[numerical] = self._scaler.transform(samples_df[numerical])
         predictions = list(np.exp(self._model.predict(samples_df)))
+
         return predictions
         
